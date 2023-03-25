66ba4b0cc487c977a15fe509f591dee96f6bba09
surajratnaparkhi@gmail.com <surajratnaparkhi@gmail.com>
2023-03-25 23:23:38 +0530

    h2 db created

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -8,9 +8,54 @@ import jakarta.persistence.GenerationType;
 public class Car
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @GeneratedValue
+    private long idc;
 
     private String brand, model, color, registerNumber;
-    private int year, price;
+    private int years, price;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getRegisterNumber() {
+		return registerNumber;
+	}
+	public void setRegisterNumber(String registerNumber) {
+		this.registerNumber = registerNumber;
+	}
+	public int getYears() {
+		return years;
+	}
+	public void setYears(int years) {
+		this.years = years;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Car(String brand, String model, String color, String registerNumber, int years, int price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.years = years;
+		this.price = price;
+	}
 }
